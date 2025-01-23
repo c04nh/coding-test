03e91e22f4aae3deceecc5fa80de479ca7a18ef8
Nahyun Cho <nhsally04@gmail.com>
2025-01-23 15:32:01 +0900

    [Bronze V] Title: A+B - 7, Time: 204 ms, Memory: 18636 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int[][] str = new int[N][2];
+        
+        for(int i = 0; i < N; i++){
+            str[i][0] = scan.nextInt();
+            str[i][1] = scan.nextInt();
+        }
+        
+        for(int i = 0; i < N; i++){
+            System.out.println("Case #" + (i+1) + ": " + (str[i][0] + str[i][1]));
+        }
+        
+    }
+}
