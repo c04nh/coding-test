551b8788a317c0c0e3ef5b4f556243b5607b2636
Nahyun Cho <nhsally04@gmail.com>
2025-01-24 00:42:48 +0900

    [Bronze V] Title: A+B - 5, Time: 188 ms, Memory: 17904 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        ArrayList<int[]> list = new ArrayList<int[]>();;
+        
+        while(true){
+            int[] nums = new int[2];
+            nums[0] = scan.nextInt();
+            nums[1] = scan.nextInt();
+            if(nums[0] == 0 && nums[1] == 0){
+                break;
+            }
+            list.add(nums);
+        }
+        
+        for(int[] arr : list){
+            System.out.println(arr[0] + arr[1]);
+        }
+        
+    }
+}
