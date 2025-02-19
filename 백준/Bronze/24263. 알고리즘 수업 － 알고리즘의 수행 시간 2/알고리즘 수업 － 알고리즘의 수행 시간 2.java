5d9cea3dcd56848eb48ac713aa6b34f33f3de1bc
Nahyun Cho <nhsally04@gmail.com>
2025-02-20 00:23:00 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 176 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
