a0dcb4b6ef35960884400c2a410dbb3957d071c0
Nahyun Cho <nhsally04@gmail.com>
2025-02-25 00:12:20 +0900

    [Bronze II] Title: 수 정렬하기, Time: 284 ms, Memory: 22228 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int[] arr = new int[N];
+        
+        for(int i = 0; i < N; i++){
+            arr[i] = scan.nextInt();
+        }
+        
+        for(int i = 0; i < N-1; i++){
+            for(int j = i+1; j < N; j++){
+                if(arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        
+        for(int i = 0; i < N; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
