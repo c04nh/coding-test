01f9ffe4fdbb773347cc83e3708757775705e321
Nahyun Cho <nhsally04@gmail.com>
2025-03-21 12:11:11 +0900

    [Bronze III] Title: 팩토리얼, Time: 172 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int answer = 1;
+        
+        for(int i = 1; i <= N; i++){
+            answer *= i;
+        }
+        
+        System.out.print(answer);
+    }
+}
