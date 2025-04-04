644edc62bf04d08a3621bf34495ebcab7b9cfa20
Nahyun Cho <nhsally04@gmail.com>
2025-04-04 14:59:43 +0900

    [Bronze V] Title: HI-ARC=?, Time: 176 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int H = scan.nextInt();
+        int I = scan.nextInt();
+        int A = scan.nextInt();
+        int R = scan.nextInt();
+        int C = scan.nextInt();
+
+        System.out.print((H*I) - (A*R*C));
+    }
+}
