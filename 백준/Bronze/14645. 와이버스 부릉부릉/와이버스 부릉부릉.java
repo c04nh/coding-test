915184a8486d54f181b1313bd1685e9eff9eafcd
Nahyun Cho <nhsally04@gmail.com>
2025-04-04 10:24:21 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 172 ms, Memory: 17324 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int K = scan.nextInt();
+
+        for(int i = 0; i < N; i++){
+            int A = scan.nextInt();
+            int B = scan.nextInt();
+        }
+        
+
+        System.out.print("비와이");
+    }
+}
