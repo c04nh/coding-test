2f4a95a8099d6438dfd81605cd994c8540cc36b5
Nahyun Cho <nhsally04@gmail.com>
2025-04-04 10:34:17 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅂ, Time: 1292 ms, Memory: 32336 KB -BaekjoonHub

---
+++ 백준/Bronze/23808. 골뱅이 찍기 － ㅂ/골뱅이 찍기 － ㅂ.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+
+        for(int i = 1; i <= 2 * N; i++){
+            for(int j = 1; j <= N; j++){
+                System.out.print("@");
+            }
+            for(int j = 1; j <= 3 * N; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= N; j++){
+                System.out.print("@");
+            }
+            System.out.println();
+        }
+
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= 5 * N; j++){
+                System.out.print("@");
+            }
+            System.out.println();
+        }
+
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= N; j++){
+                System.out.print("@");
+            }
+            for(int j = 1; j <= 3 * N; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= N; j++){
+                System.out.print("@");
+            }
+            System.out.println();
+        }
+
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= 5 * N; j++){
+                System.out.print("@");
+            }
+            System.out.println();
+        }
+    }
+}
