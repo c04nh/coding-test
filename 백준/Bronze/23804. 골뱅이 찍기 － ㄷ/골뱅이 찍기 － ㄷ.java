98f428ab7d941ccb3736d4219f8c856ce9795778
Nahyun Cho <nhsally04@gmail.com>
2025-04-07 23:23:17 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄷ, Time: 864 ms, Memory: 27712 KB -BaekjoonHub

---
+++ 백준/Bronze/23804. 골뱅이 찍기 － ㄷ/골뱅이 찍기 － ㄷ.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N * 5; j++){
+                System.out.print("@");
+            }
+            System.out.println();
+        }
+
+        for(int i = 0; i < N * 3; i++){
+            for(int j = 0; j < N; j++){
+                System.out.print("@");
+            }
+            System.out.println();
+        }
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N * 5; j++){
+                System.out.print("@");
+            }
+            System.out.println();
+        }
+    }
+}
