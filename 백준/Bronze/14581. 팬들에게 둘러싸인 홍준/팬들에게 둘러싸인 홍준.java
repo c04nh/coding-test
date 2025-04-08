67303c253c416cd70a5d55d6a9eb291d1cb20185
Nahyun Cho <nhsally04@gmail.com>
2025-04-08 22:15:25 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 180 ms, Memory: 18360 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String id = scan.next();
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + id + "::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
