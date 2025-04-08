9333b76fdd99efe544a3ac621af9fe20cd369a72
Nahyun Cho <nhsally04@gmail.com>
2025-04-08 21:54:28 +0900

    [Bronze V] Title: 연세대학교, Time: 172 ms, Memory: 17524 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        
+        if(N == 0) System.out.print("YONSEI");
+        else System.out.print("Leading the Way to the Future");
+    }
+}
