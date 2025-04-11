8dd9652a6f23623f72d218795de63fbffab51bdd
Nahyun Cho <nhsally04@gmail.com>
2025-04-11 10:23:20 +0900

    [Bronze V] Title: 오늘 날짜, Time: 164 ms, Memory: 16876 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.time.*;
+
+class Main {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+
+        System.out.print(now);
+        
+    }
+}
