3c229c35ebfcd24b886bff5249b007ad02ff4ee3
Nahyun Cho <nhsally04@gmail.com>
2025-04-11 10:34:20 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 180 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+
+        switch(N){
+            case 1:
+                System.out.println(11);
+                System.out.print("A B C D E F G H J L M");
+                break;
+            case 2:
+                System.out.println(9);
+                System.out.print("A C E F G H I L M");
+                break;
+            case 3:
+                System.out.println(9);
+                System.out.print("A C E F G H I L M");
+                break;
+            case 4:
+                System.out.println(9);
+                System.out.print("A B C E F G H L M");
+                break;
+            case 5:
+                System.out.println(8);
+                System.out.print("A C E F G H L M");
+                break;
+            case 6:
+                System.out.println(8);
+                System.out.print("A C E F G H L M");
+                break;
+            case 7:
+                System.out.println(8);
+                System.out.print("A C E F G H L M");
+                break;
+            case 8:
+                System.out.println(8);
+                System.out.print("A C E F G H L M");
+                break;
+            case 9:
+                System.out.println(8);
+                System.out.print("A C E F G H L M");
+                break;
+            case 10:
+                System.out.println(8);
+                System.out.print("A B C F G H L M");
+                break;
+        }
+        
+    }
+}
