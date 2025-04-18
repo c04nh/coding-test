be28276d92fd39f1d1f936cf9fbb40e4a011232a
Nahyun Cho <nhsally04@gmail.com>
2025-04-18 22:17:09 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 184 ms, Memory: 17816 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double d1 = scan.nextDouble() * 2;
+        double d2 = scan.nextDouble() * 2 * 3.141592;
+
+        System.out.print(d1 + d2);
+    }
+}
