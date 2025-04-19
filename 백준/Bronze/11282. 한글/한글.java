079c18ccc4d935d4f24a1675cd39d8d2edcee6d1
Nahyun Cho <nhsally04@gmail.com>
2025-04-19 19:55:31 +0900

    [Bronze IV] Title: 한글, Time: 188 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/11282. 한글/한글.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt() - 1;
+
+        System.out.print((char)(N + '가'));
+    }
+}
