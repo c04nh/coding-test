cabc617a60b2e0a47301d0dc6a0b04db2540163e
Nahyun Cho <nhsally04@gmail.com>
2025-05-08 23:07:38 +0900

    [Bronze IV] Title: 모비스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28074. 모비스/모비스.py
@@ -0,0 +1,6 @@
+strInput = input()
+
+if(strInput.count('M') > 0 and strInput.count('O') and strInput.count('B') and strInput.count('I') and strInput.count('S')):
+    print("YES")
+else:
+    print("NO")
