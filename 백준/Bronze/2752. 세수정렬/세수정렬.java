f7a380e996a0f6e6401aba61bd0e1e2638a02c07
Nahyun Cho <nhsally04@gmail.com>
2025-05-27 23:11:14 +0900

    [Bronze IV] Title: 세수정렬, Time: 188 ms, Memory: 18304 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] arr = new int[3];
+
+        for(int i = 0; i < 3; i++){
+            arr[i] = scan.nextInt();
+        }
+
+        for(int i = 0; i < 2; i++){
+            for(int j = 1; j < 3; j++){
+                if(arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        for(int i = 0; i < 3; i++){
+            System.out.print(arr[i]+ " ");
+        }
+        
+    }
+}
