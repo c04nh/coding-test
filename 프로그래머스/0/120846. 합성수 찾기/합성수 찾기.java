1b2a88f7567edeb179edd6a6e3317073c9bf0132
Nahyun Cho <nhsally04@gmail.com>
2025-10-16 18:44:57 +0900

    [level 0] Title: 합성수 찾기, Time: 0.16 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i = 4; i <= n; i++){
+            int div = 0;
+            for(int j = 2; j <= i; j++){
+                if(i % j == 0) div++;
+            }
+            
+            if(div >= 2) answer++;
+        }
+        return answer;
+    }
+}
