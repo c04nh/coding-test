49d07c97b04b2e883c5aca166fa42f06e01c129f
Nahyun Cho <nhsally04@gmail.com>
2025-12-08 00:19:08 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -0,0 +1,26 @@
+import math
+
+def solution(keyinput, board):
+    answer = [0, 0]
+    
+    for i in keyinput:
+        if i == "left":
+            answer[0] -= 1
+        elif i == "right":
+            answer[0] += 1
+        elif i == "up":
+            answer[1] += 1
+        elif i == "down":
+            answer[1] -= 1
+        
+        if answer[0] > board[0]/2:
+            answer[0] = math.floor(board[0]/2)
+        elif answer[0] < -board[0]/2:
+            answer[0] = math.ceil(-board[0]/2)
+        if answer[1] > board[1]/2:
+            answer[1] = math.floor(board[1]/2)
+        elif answer[1] < -board[1]/2:
+            answer[1] = math.ceil(-board[1]/2)
+    
+    
+    return answer
