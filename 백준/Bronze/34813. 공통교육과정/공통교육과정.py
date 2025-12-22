d63ca6ec311e3b2d633b81a795d1026bd07cd9c0
Nahyun Cho <nhsally04@gmail.com>
2025-12-23 01:11:51 +0900

    [Bronze V] Title: 공통교육과정, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.py
@@ -0,0 +1,10 @@
+subject = input()
+
+if subject[0] == "F":
+    print("Foundation")
+elif subject[0] == "C":
+    print("Claves")
+elif subject[0] == "V":
+    print("Veritas")
+elif subject[0] == "E":
+    print("Exploration")
