90180c43901b7809a6bbfc7b87f79611decfefc3
Nahyun Cho <nhsally04@gmail.com>
2026-03-14 23:19:21 +0900

    [Bronze III] Title: 직각삼각형, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,10 @@
+while True:
+  a, b, c = map(int,input().split())
+  if a == 0 and b == 0 and c == 0:
+    break
+  arr = sorted([a, b, c])
+
+  if arr[0]*arr[0] + arr[1]*arr[1] == arr[2]*arr[2]:
+    print("right")
+  else:
+    print("wrong")
