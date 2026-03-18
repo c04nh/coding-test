a5fb417558909ad830a9e94f528c29a6e648b59e
Nahyun Cho <nhsally04@gmail.com>
2026-03-18 12:40:56 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
@@ -0,0 +1,16 @@
+N = int(input())
+arr = list(map(int, input().split()))
+
+y = 0
+m = 0
+
+for i in arr:
+  y += ((i // 30) + 1) * 10
+  m += ((i // 60) + 1) * 15
+
+if y < m:
+  print('Y', y)
+elif y > m:
+  print('M', m)
+else:
+  print('Y M', y)
