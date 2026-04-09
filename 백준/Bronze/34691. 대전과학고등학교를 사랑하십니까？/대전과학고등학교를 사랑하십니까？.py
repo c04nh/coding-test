835782177a4d2f610a63dc66f0d74704595c3dc4
Nahyun Cho <nhsally04@gmail.com>
2026-04-09 12:05:06 +0900

    [Bronze V] Title: 대전과학고등학교를 사랑하십니까?, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.py
@@ -0,0 +1,11 @@
+while True:
+    n = input()
+    if n == 'end':
+        break
+    
+    if n == 'animal':
+        print("Panthera tigris")
+    elif n == 'tree':
+        print("Pinus densiflora")
+    elif n == 'flower':
+        print("Forsythia koreana")
