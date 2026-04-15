88693bcdc011d13f81aef97f5c4f2835eaede5e0
Nahyun Cho <nhsally04@gmail.com>
2026-04-15 12:10:08 +0900

    [Bronze V] Title: Football Scoring, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.py
@@ -0,0 +1,10 @@
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+A_total = A[0]*6 + A[1]*3 + A[2]*2 + A[3]*1 + A[4]*2
+B_total = B[0]*6 + B[1]*3 + B[2]*2 + B[3]*1 + B[4]*2
+
+print(A_total, B_total)
+
+
+
