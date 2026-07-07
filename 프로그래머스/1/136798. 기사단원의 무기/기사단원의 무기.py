a9e2b47a12ce1af1692693b27858a266a5f6c394
Nahyun Cho <nhsally04@gmail.com>
2026-07-07 15:17:20 +0900

    [level 1] Title: 기사단원의 무기, Time: 844.88 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,19 @@
+def solution(number, limit, power):
+    answer = 0
+    
+    for i in range(1, number+1):
+        cnt = 0
+        for j in range(1, int(i**0.5)+1):
+            if j*j == i:
+                cnt += 1
+                continue
+
+            if i % j == 0:
+                cnt += 2
+        
+        if cnt > limit:
+            answer += power
+        else:
+            answer += cnt
+        
+    return answer
