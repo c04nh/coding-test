0a4e93fe5fad042108e68be2e1464a1b81774d1c
Nahyun Cho <nhsally04@gmail.com>
2026-07-09 13:09:01 +0900

    [level 1] Title: 덧칠하기, Time: 2.10 ms, Memory: 12.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -0,0 +1,10 @@
+def solution(n, m, section):
+    answer = 0
+    last = 0
+    
+    for i in section:
+        if i > last:
+            answer += 1
+            last = i + m - 1
+    
+    return answer
