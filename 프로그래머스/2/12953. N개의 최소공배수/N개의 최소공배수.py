d49982dcf7d3e7e983c8bdc8f6b92fb92f3da2c3
Nahyun Cho <nhsally04@gmail.com>
2026-09-16 12:20:09 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -0,0 +1,13 @@
+import math
+
+def lcm(a, b):
+    return (a*b) // math.gcd(a, b)
+
+
+def solution(arr):
+    answer = arr[0]
+    
+    for i in arr:
+        answer = lcm(answer, i)
+        
+    return answer
