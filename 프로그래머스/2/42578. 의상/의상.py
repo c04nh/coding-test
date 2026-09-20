9a806d1e86a262ec35869c68b2cf1b4c803b8ab3
Nahyun Cho <nhsally04@gmail.com>
2026-09-21 00:16:58 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -0,0 +1,16 @@
+def solution(clothes):
+    answer = 1
+    dic = {}
+    
+    for i in clothes:
+        if i[1] not in dic:
+            dic[i[1]] = 1
+        else:
+            dic[i[1]] += 1
+
+    for i in dic:
+        answer *= dic[i] + 1
+        
+    answer -= 1
+    
+    return answer
